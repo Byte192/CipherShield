1dc04d5ff8c2afbb1c65802255043dcb8775867d
eaklein022 <eaklein022@student.wethinkcode.co.za>
2023-06-29 13:52:21 +0200

    Added Encryption and Decryption classes

--- alpha/src/main/java/com/division/cyber/Decryption.java
+++ alpha/src/main/java/com/division/cyber/Decryption.java
@@ -7,27 +7,29 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Base64;
 
-public class Encryption {
-    public static final String ENCRYPTION_ALGORITHM = "AES/GCM/NoPadding";
+public class Decryption {
+    private static final String ENCRYPTION_ALGORITHM = "AES/GCM/NoPadding";
     private static final int TAG_LENGTH_BIT = 128;
     private static final int IV_LENGTH_BYTE = 12;
     private static final int KEY_LENGTH_BIT = 256;
 
-    public static String encrypt(String key, String data) throws Exception {
+    public static String decrypt(String key, String encryptedData) throws Exception {
         byte[] keyBytes = generateKeyBytes(key);
-        byte[] iv = generateRandomBytes(IV_LENGTH_BYTE);
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedData);
+        byte[] iv = new byte[IV_LENGTH_BYTE];
+        byte[] encryptedContent = new byte[encryptedBytes.length - IV_LENGTH_BYTE];
+
+        System.arraycopy(encryptedBytes, 0, iv, 0, IV_LENGTH_BYTE);
+        System.arraycopy(encryptedBytes, IV_LENGTH_BYTE, encryptedContent, 0, encryptedContent.length);
 
         Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
         SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
         GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(TAG_LENGTH_BIT, iv);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, gcmParameterSpec);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, gcmParameterSpec);
 
-        byte[] encryptedBytes = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
-        byte[] encryptedData = new byte[iv.length + encryptedBytes.length];
-        System.arraycopy(iv, 0, encryptedData, 0, iv.length);
-        System.arraycopy(encryptedBytes, 0, encryptedData, iv.length, encryptedBytes.length);
+        byte[] decryptedBytes = cipher.doFinal(encryptedContent);
 
-        return Base64.getEncoder().encodeToString(encryptedData);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
     }
 
     private static byte[] generateKeyBytes(String key) throws Exception {
@@ -38,21 +40,16 @@ public class Encryption {
         return keyBytes;
     }
 
-    private static byte[] generateRandomBytes(int length) {
-        byte[] bytes = new byte[length];
-        new java.security.SecureRandom().nextBytes(bytes);
-        return bytes;
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try {
             String key = "MySecretKey";
-            String data = "Hello, World!";
+            String encryptedData = "QFHZX7fQMz73Ip8+vj7lKBFyfBxa06u51srh7kYEsZP4LHdQd6l/pxc=";
 
-            String encryptedData = encrypt(key, data);
-            System.out.println("Encrypted data: " + encryptedData);
-        } catch (Error e) {
+            String decryptedData = decrypt(key, encryptedData);
+            System.out.println("Decrypted data: " + decryptedData);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
